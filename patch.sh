e72619984e32dfeea6eb49e83722eabd9b8baaca
Kyle Harrison <khwebmail@gmail.com>
2018-09-13 11:36:44 +0100

    manifest: Updates
    - Remove manifest entries & patches for things now merged in LOS

--- patch.sh
+++ patch.sh
@@ -27,14 +27,8 @@ repopick 223413
 # android_external_tinycompress
 repopick 225762-225763 223008 223011;
 
-# android_frameworks_opt_telephony
-repopick 223774
-
 # android_hardware_interfaces
 repopick 224064
-
-# android_hardware_libhardware
-repopick 223097
 
 # android_system_bt
 repopick 223945 225422 225423
@@ -69,17 +63,7 @@ repopick -t pie-wlan-caf
 # android_hardware_qcom_fm
 repopick 226683-226743 223685 226744 226745 223678 223683 223684 226862 223686 223687 223692 223688 224246 224386
 
-repopick -t pie-gralloc_10_usage_bits
 repopick -t per-process-sdk-override
-
-# Command line tools
-repopick -t pie-cmdline-tools -e 223893,224028
 
 # Styles
 repopick -t pie-styles -e 225583,225755
-cd frameworks/base
-git am -3 $TOP/0001-DNM-Temp-build-fix-for-pie-styles.patch
-cd $TOP
-
-# Advanced restart
-repopick -t pie-powermenu
