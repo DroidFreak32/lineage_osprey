2ef193b7e77469d180044c8c89d57f7cc987c074
DroidFreak32 <rushabshah32@gmail.com>
2018-11-09 16:06:23 +0530

    moar update

--- idk.sh
+++ idk.sh
@@ -1,25 +1,15 @@
-#repopick -c 30 -t pie-touchscreen-gestures
-#repopick -c 30 -t pie-custom-charging-sounds
 repopick -c 30 -t pie-powermenu
 repopick -c 30 -t pie-styles
-# repopick  225983
-#repopick -c 30 -t pie-display-rotation
-#repopick -c 30 -t pie-navbar-runtime-toggle
-#repopick -c 30 -t pie-buttons
+repopick -c 30 226148
 repopick -c 30 -t pie-su
-#repopick -c 30 -t pie-swap-volume-buttons
-#repopick -c 30 -t pie-kill-app
-#repopick -c 30 -t pie-tiles
-repopick -c 30 -t pie-manual-provisioning
-repopick -c 30 -t pie-privacy-guard
+#repopick -c 30 226154
+#repopick -c 30 -t pie-manual-provisioning
+#repopick -c 30 -t pie-privacy-guard
 repopick -c 30 -t p-display-shrink
-repopick -c 30 -t lineagehw-hidl
-#repopick -c 30 -t pie-keyboard
-#repopick -c 30 -t trust-vendor-patch
+#repopick -c 30 -t lineagehw-hidl
 repopick -c 30 -t pie-network-traffic
-#repopick -c 30 -t pie-camera2
 repopick -c 30 229612 226343 227821 226176 226600 231823 231824 233717
-repopick -c 30 -t pie-msim-ringtones
-repopick -c 30 -t pie-expanded-desktop
+#repopick -c 30 -t pie-msim-ringtones
+#repopick -c 30 -t pie-expanded-desktop
 #Topic
 #pie-display-rotation
